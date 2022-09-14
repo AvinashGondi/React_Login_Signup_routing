487d8a364b5a75554b205f9351a7285c8611077d
Avinash <agondi@keypoint-tech.com>
2022-09-14 17:00:23 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import Login from './components/login/login';
+
+const App = () => (
+  <div>
+    <Main />
+  </div>
+);
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/login/login';
+import SignUp from './components/signup/signup';
+import Home from './Home';
+import Test from './Test';
+
+// The Main component renders one of the three provided
+// Routes (provided that one matches). Both the /roster
+// and /schedule routes will match any pathname that starts
+// with /roster or /schedule. The / route will only match
+// when the pathname is exactly the string "/"
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
+    </Switch>
+  </main>
+);
+
+export default Main;
